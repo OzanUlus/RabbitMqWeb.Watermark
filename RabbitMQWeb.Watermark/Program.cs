43aee7dd5566da3297d5641e3fd58294f2274a1b
OzanUlus <ilkerozanulus@gmail.com>
2024-06-06 16:39:26 +0300

    created appdbcontext

--- RabbitMQWeb.Watermark/Program.cs
+++ RabbitMQWeb.Watermark/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using RabbitMQWeb.Watermark.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseInMemoryDatabase(databaseName:"ProductDb");
+});
 
 var app = builder.Build();
 
